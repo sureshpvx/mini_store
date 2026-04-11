d91207fcc6970148170fe4fe3118c88c68f5ecd8
sureshpvx <sureshsharma04885@gmail.com>
2026-04-11 15:03:32 +0530

    sign in sign up with design

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_admin!
   def index
+  end
+
+  private
+  def ensure_admin!
+    unless current_user.admin?
+      redirect_to root_path, status: :forbidden, alert: "Admins only!"
+    end
   end
 end
