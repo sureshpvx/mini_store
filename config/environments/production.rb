8ac2753cc80474d66cfde206fe2ff0c2e6333050
sureshpvx <sureshsharma04885@gmail.com>
2026-05-21 15:19:06 +0530

    Fix Cloudinary ActiveStorage integrity error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,9 @@ Rails.application.configure do
   # Replace the default in-process memory cache store with a durable alternative.
   config.cache_store = :solid_cache_store
 
+  config.active_storage.analyzers = []
+
+
   # Replace the default in-process and non-durable queuing backend for Active Job.
   # config.active_job.queue_adapter = :solid_queue
   # config.solid_queue.connects_to = { database: { writing: :queue } }
