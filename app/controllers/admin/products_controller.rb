47c3a7d3a1296b530a456e61a3a5876a5da9112d
sureshpvx <sureshsharma04885@gmail.com>
2026-05-23 14:50:50 +0530

    image upload solve 11

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -18,48 +18,25 @@ class Admin::ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+
+    # Check if files are attached before saving
     files = params.dig(:product, :images).to_a.reject(&:blank?)
     if files.blank?
-      @product = Product.new(product_params.except(:images))
       @product.errors.add(:images, "must be attached")
-      flash.now[:alert] = "Please attach at least one image for the product"
+      flash.now[:alert] = "Please attach at least one image or video for the product"
       load_categories
       render :new, status: :unprocessable_entity
       return
     end
 
-    @product = nil
-    success = false
-
-    ActiveRecord::Base.transaction do
-      @product = Product.new(product_params.except(:images))
-      unless @product.save
-        raise ActiveRecord::Rollback
-      end
-
-      @product.images.attach(files)
-      # if attachments aren't present for some reason, rollback
-      unless @product.images.attached?
-        Rails.logger.error "Attachment failed for product creation"
-        @product.errors.add(:images, "failed to attach")
-        raise ActiveRecord::Rollback
-      end
-
-      success = true
-    end
-
-    unless success
-      puts "Product create aborted in transaction"
-      Rails.logger.warn "Product create aborted in transaction (admin=#{current_user&.id})"
-      # Rebuild a new product instance for rendering form with errors where possible
-      @product ||= Product.new(product_params.except(:images))
+    if @product.save
+      redirect_to admin_product_path(@product), notice: "Product created successfully."
+    else
       load_categories
-      flash.now[:alert] ||= "There was a problem creating the product and attaching images."
+      flash.now[:alert] = "There was a problem creating the product."
       render :new, status: :unprocessable_entity
-      return
     end
-
-    redirect_to admin_product_path(@product), notice: "Product created"
   end
   def show
   end
