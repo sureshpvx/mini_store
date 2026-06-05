819c7c04eecf226c1f346f1aa7c212becb67b6d6
sureshpvx <sureshsharma04885@gmail.com>
2026-06-05 15:16:39 +0530

    solving assets issue in production

--- render-build.sh
+++ render-build.sh
@@ -2,9 +2,8 @@
 set -o errexit
 
 bundle install
-
+bin/rails tailwindcss:build
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-
 bundle exec rails db:migrate
-bundle exec rails search:setup  # safe to run every time, only rebuilds if empty
+bundle exec rails search:setup
