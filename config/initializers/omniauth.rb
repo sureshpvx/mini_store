33fbd1828005295de1bcb64dad20ccafb4f1aecd
sureshpvx <sureshsharma04885@gmail.com>
2026-06-13 17:16:56 +0530

    fix auth issue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,17 +1,20 @@
 # config/initializers/omniauth.rb
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-           ENV.fetch('GOOGLE_CLIENT_ID'),
-           ENV.fetch('GOOGLE_CLIENT_SECRET'),
-           {
-             scope: 'email,profile',
-             prompt: 'select_account',
-             image_aspect_ratio: 'square',
-             image_size: 50,
-             access_type: 'online'
-           }
+
+if ENV["GOOGLE_CLIENT_ID"].present? && ENV["GOOGLE_CLIENT_SECRET"].present?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :google_oauth2,
+             ENV["GOOGLE_CLIENT_ID"],
+             ENV["GOOGLE_CLIENT_SECRET"],
+             {
+               scope: "email,profile",
+               prompt: "select_account",
+               image_aspect_ratio: "square",
+               image_size: 50,
+               access_type: "online"
+             }
+  end
 end
 
-OmniAuth.config.on_failure = Proc.new { |env|
+OmniAuth.config.on_failure = proc do |env|
   OmniAuth::FailureEndpoint.new(env).redirect_to_failure
-}
+end
