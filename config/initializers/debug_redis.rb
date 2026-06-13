c7e514fe51ce3b820af6f1e95e64bd33347a3f8f
sureshpvx <sureshsharma04885@gmail.com>
2026-06-13 16:01:25 +0530

    cleanup

--- config/initializers/debug_redis.rb
+++
@@ -1,3 +0,0 @@
-# config/initializers/debug_redis.rb
-
-Rails.logger.info "REDIS_URL=#{ENV['REDIS_URL'].inspect}"
--- config/initializers/redis_test.rb
+++
@@ -1,8 +0,0 @@
-# config/initializers/redis_test.rb
-
-begin
-  redis = Redis.new(url: ENV["REDIS_URL"])
-  Rails.logger.info "Redis ping: #{redis.ping}"
-rescue => e
-  Rails.logger.error "Redis connection failed: #{e.message}"
-end
