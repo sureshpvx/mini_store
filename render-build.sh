5fc27b4ceb28c7e764f3ca8dd3704d14eb4d02d0
sureshpvx <sureshsharma04885@gmail.com>
2026-06-13 18:33:14 +0530

    remover unwanted code

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-bundle install
-bin/rails tailwindcss:build
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
-bundle exec rails search:setup
--- render-start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-bundle exec sidekiq -C config/sidekiq.yml &
-bundle exec puma -C config/puma.rb -p ${PORT:-3000}
